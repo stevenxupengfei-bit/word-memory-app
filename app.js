0a7d05467718020efb20ae39593baf602669ce48
stevenxupengfei-bit <steven.xupengfei@gmail.com>
2026-09-09 18:09:04 +0800

    Tighten mobile study controls

--- app.js
+++ app.js
@@ -125,7 +125,6 @@ function bindEvents() {
   $("logoutBtn").addEventListener("click", logout);
   $("importBtn").addEventListener("click", openImport);
   $("emptyImportBtn").addEventListener("click", openImport);
-  $("mobileImportBtn").addEventListener("click", openImport);
   $("closeImportBtn").addEventListener("click", closeImport);
   $("cancelImportBtn").addEventListener("click", closeImport);
   $("confirmImportBtn").addEventListener("click", importWords);
